b33581d490521775ac40fdd50ab7d9f5815a443e
Jairo Soares <jairo.soares@gmail.com>
2026-09-17 14:20:57 -0300

    chore: initial commit

---
+++ src/main/java/academy/jairo/expensegate/SpringExpenseGateApplication.java
@@ -0,0 +1,13 @@
+package academy.jairo.expensegate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExpenseGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExpenseGateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/academy/jairo/expensegate/SpringExpenseGateApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.jairo.expensegate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExpenseGateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
